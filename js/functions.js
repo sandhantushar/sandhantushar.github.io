54960852576ae34ad2579344853d4b0d004a4f17
Tushar Sandhan <sandhantushar@gmail.com>
2022-02-18 18:58:45 +0530

    Delete all
    delete all folder

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var nav = document.getElementById("myTopnav");
-    if (nav.className === "navbar") {
-      nav.className += " responsive";
-    } else {
-      nav.className = "navbar";
-    }
-  }
